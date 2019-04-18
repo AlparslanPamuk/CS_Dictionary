ea502aaf9ec5a9657c3267dd03f68105a73822e6
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-04-18 15:08:31 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace CS_Dictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "English-->";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.YellowGreen;
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(20, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 59);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(298, 20);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(300, 228);
+            this.listBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(101, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(154, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Turkish-->";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Orange;
+            this.button2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button2.Location = new System.Drawing.Point(101, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 59);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "UPDATE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkRed;
+            this.button3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Location = new System.Drawing.Point(180, 165);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 59);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "DELETE";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(601, 252);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;    // access veri tabanı kodlarını çağırmamızı sağlar
+
+namespace CS_Dictionary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        OleDbConnection connection = new OleDbConnection("Provider=Microsoft.JET.OleDb.4.0;Data Source=" 
+                                                         + Application.StartupPath + "\\db_dictionary.mdb");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            OleDbCommand addCommand = new OleDbCommand("insert into engturk (english,turkish) values('" + textBox1.Text + "','" +
+                textBox2.Text + "')",connection);
+            addCommand.ExecuteNonQuery(); // ekle komutunun tanımlandığı sorguyu çalıştır, sonuçları veri tabanına işle.
+            connection.Close();
+            MessageBox.Show("Word has been added to DataBase.", "DataBase Movements");
+            textBox1.Clear();
+            textBox2.Clear();
+            try
+            {
+
+            }
+            catch (Exception explanation)
+            {
+                MessageBox.Show(explanation.Message, "DataBase Movements");
+                connection.Close();    //---------------hata olursa yuardaki bağlantı kapatma çalışmayacak o yüzdenburda kapatıyoruz-----------
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                connection.Open();
+                OleDbCommand updateCommand = new OleDbCommand("update engturk set turkish= '" + textBox2.Text + "' where english = '"
+                    + textBox1.Text + "'", connection);
+                updateCommand.ExecuteNonQuery();// güncelle komutunun tanımlandığı sorguyu çalıştır, sonuçları veri tabanına işle.
+                connection.Close();
+                MessageBox.Show("Word has been updated on DataBase.", "DataBase Movements");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            catch (Exception explanation)
+            {
+                MessageBox.Show(explanation.Message, "DataBase Movements");
+                connection.Close();    //---------------hata olursa yuardaki bağlantı kapatma çalışmayacak o yüzdenburda kapatıyoruz-----------
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                OleDbCommand deletecommand = new OleDbCommand("delete from engturk where english = '" + textBox1.Text + "'", connection);
+                deletecommand.ExecuteNonQuery();
+                connection.Close();
+                MessageBox.Show("Word has been deleted from DataBase", "DataBase Movements");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            catch (Exception explanation)
+            {
+            MessageBox.Show(explanation.Message, "Databese Movements");
+                connection.Close();
+                
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.Items.Clear();
+                connection.Open();
+                OleDbCommand searchcommand = new OleDbCommand("select english,turkish from engturk where english like'" +
+                    textBox1.Text + "%'", connection);
+                // ---------------girilen harfle ilgili her kaydı getirir-------------
+                OleDbDataReader read = searchcommand.ExecuteReader();
+                //------------ veri tabanından veri okumayı gerçekleştirir, okuma işlemi gerçekleştirir |Tablodaki kayıtların okunmasını sağlıyoruz|---------------
+                while (read.Read()) //------Eğer herhangibi kayıt bulunmuşsa: ----------
+                {
+                    listBox1.Items.Add(read["english"].ToString() + "=" + read["turkish"].ToString()); // bir kelime aranıp bulunmuşsa listbox 1'e ingilizce kaydın değerini yazdırır,----
+                    //-----------Eşittir'den sonra türkçe karşılığını yazdırır. ---------------
+                }
+                connection.Close();
+            }
+            catch (Exception explanation)
+            {
+                MessageBox.Show(explanation.Message, "DataBase Movements");
+                connection.Close();
+            }
+        }
+    }
+}
